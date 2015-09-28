24d7244febd66d475ee74a9102fab1599d6b73d4
Mikhail Itkin <mikhail.itkin@met.no>
2015-09-28 15:25:31 +0200

    fix local_gac_dir variable

--- features/environment.py
+++ features/environment.py
@@ -10,7 +10,7 @@ def before_all(context):
         playbook = playbook.read()
     context.playbook = yaml.safe_load(playbook)
     context.test_date_string = context.playbook[0]['vars']['test_date']
-    context.local_avhrr_dir = context.playbook[0]['vars']['local_gac_dir']
-    context.local_nsidc_dir = context.playbook[0]['vars']['local_sic_dir']
+    context.local_gac_dir = context.playbook[0]['vars']['local_gac_dir']
+    context.local_sic_dir = context.playbook[0]['vars']['local_sic_dir']
 
     context.test_date = dateutil_parser.parse(context.test_date_string)
