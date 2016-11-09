7a42b53dce73dbfbb210fa5ccd0217ef29516815
Mikhail Itkin <itkin.m@gmail.com>
2016-11-09 14:29:40 +0000

    switch to gac v2

--- env/lustre-env.sh
+++ env/lustre-env.sh
@@ -2,4 +2,5 @@ export PROJECT_DIR=/lustre/storeB/users/mikhaili/icearc-avhrr-sic
 export COMPUTE_SIC_DIR=${PROJECT_DIR}/codeshop/compute_sic
 export SGE_DIR=${PROJECT_DIR}/sge
 export RESAMPLED_GAC_DIR=${PROJECT_DIR}/data/resampled-avhrr-gac
-export GACDIR=/lustre/storeB/users/steinare/gacrepr1/pps_v1
+# export GACDIR=/lustre/storeA/project/fou/hi/normap/gacrepr_v1/pps_v1
+export GACDIR=/lustre/storeA/project/fou/hi/normap2/gacrepr_v2/pps_v2/
